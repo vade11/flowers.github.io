e5fab6c78f629f51e178ab50da47ce255d7b5502
vade11 <149937040+vade11@users.noreply.github.com>
2023-11-06 06:19:13 +0700

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+  func main () {
+
+	var bilangan int
+	var hasil bool
+
+	fmt.Scan(&bilangan)
+
+	hasil=(bilangan % 2 == 1 bilangan < 0 )
+	
+	fmt.Print(hasil)
+
+
+
+  }
